246195ddab12cc5470a38aba66632a5b69b8126c
Daniel Mobley <djmobley7@gmail.com>
2021-06-10 17:10:19 -0400

    incremental changes

--- basics_variables-datatypes-operator-and-functions/assets/scripts/app.js
+++ basics_variables-datatypes-operator-and-functions/assets/scripts/app.js
@@ -1,9 +1,59 @@
 
 const defaultResult = 0;
 let currentResult = defaultResult;
+let log = [];
 
-let calculation = currentResult + ' + 10 = 0';
+function add() {
+    const oldResult = currentResult;
+    const userInput = getUserInput();
+    currentResult += userInput;
+    setUserOutput('+', currentResult, userInput);
 
-currentResult = currentResult + 10;
+    const logEntry = {
+        operator: 'ADD',
+        value: userInput
+    };
 
-outputResult(currentResult, calculation);
+    log.push(logEntry);
+
+    // user console.log to help with debugging
+    console.log(log);
+}
+
+function subtract() {
+    const oldResult = currentResult;
+    const userInput = getUserInput();
+    currentResult -= userInput;
+    setUserOutput('-', currentResult, userInput);
+}
+
+function multiply() {
+    const oldResult = currentResult;
+    const userInput = getUserInput();
+    currentResult += userInput;
+    setUserOutput('*', currentResult, userInput);
+}
+
+function divide() {
+    const oldResult = currentResult;
+    const userInput = getUserInput();
+    currentResult /= userInput;
+    setUserOutput('/', currentResult, userInput);
+}
+
+function getUserInput() {
+    // HTML input fields are returned as strings, so we need to convert to a number type here (because we are doing numeric operations later)
+    // alternatively we can use + function in front of the string value e.g. 
+    // return +userInput.value;
+    return parseInt(userInput.value);
+}
+
+function setUserOutput(operator, oldResult, userInput) {
+    const equation = `${currentResult} ${operator} ${userInput} =`;
+    this.outputResult(currentResult, equation);
+}
+
+addBtn.addEventListener('click', /* the button event */ add /* the js function to execute */);
+subBtn.addEventListener('click', subtract);
+mulBtn.addEventListener('click', multiply);
+divBtn.addEventListener('click', divide);
