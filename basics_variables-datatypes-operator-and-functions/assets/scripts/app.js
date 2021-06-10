a7770526368fdfb5ffce44af81bc0546281635f8
Daniel Mobley <djmobley7@gmail.com>
2021-06-10 14:05:30 -0400

    first commit

---
+++ basics_variables-datatypes-operator-and-functions/assets/scripts/app.js
@@ -0,0 +1,9 @@
+
+const defaultResult = 0;
+let currentResult = defaultResult;
+
+let calculation = currentResult + ' + 10 = 0';
+
+currentResult = currentResult + 10;
+
+outputResult(currentResult, calculation);
