6bb9415b9fbca9d87aab5073c508ed2639189836
Daniel Mobley <djmobley7@gmail.com>
2021-07-21 14:02:48 -0400

    incremental changes

--- working-with-the-DOM/assets/scripts/app.js
+++ working-with-the-DOM/assets/scripts/app.js
@@ -13,8 +13,53 @@ startAddMovieButton.addEventListener('click', toggleAddMovieModel);
 
 const cancelAddMovieButton = addMovieModal.querySelector('.modal__actions .btn--passive');
 cancelAddMovieButton.addEventListener('click', () => {
+    clearMovieInputs();
     toggleAddMovieModel();
 });
+
+const movies = [];
+
+const movieInputs = addMovieModal.querySelectorAll('input');
+
+const clearMovieInputs = () => {
+    for(const movieInput of movieInputs) {
+        movieInput.value = '';
+    }
+}
+
+const updateUI = () => {
+    const text = document.querySelector('main section');
+    if (movieInputs.length > 0) {
+        text.replaceWith(document.createElement('p'));
+    }
+}
+
+// using DOM traversal to get next button - this seems really fragile to me...
+const confirmAddMovieButton = cancelAddMovieButton.nextElementSibling;
+const confirmAddMovieButtonHandler = () => {
+    const title = movieInputs[0].value.trim();
+    const imageUrl = movieInputs[1].value.trim();
+    let rating = movieInputs[2].value.trim();
+
+    if (title === '' || imageUrl === '') {
+        alert('No empty values allowed');
+        return;
+    }
+
+    rating = parseInt(rating);
+    if (isNaN(rating) || rating < 1 || rating > 5) {
+        alert('Invalid rating');
+        return;
+    }
+
+    movies.push({title, imageUrl, rating});
+    console.log(movies);
+
+    updateUI();
+    clearMovieInputs();
+    toggleAddMovieModel();
+}
+confirmAddMovieButton.addEventListener('click', confirmAddMovieButtonHandler);
 
 const backdropClickHandler = () => {
     toggleAddMovieModel(); // TODO
